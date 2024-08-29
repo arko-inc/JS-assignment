17fd3d990990139eea80ab46681010887564d1ad
Arko McCallister <arkomccallister@Arkos-MacBook-Pro.local>
2024-08-29 23:56:38 +0600

    JS assignment

---
+++ script.js
@@ -0,0 +1,36 @@
+//1//
+let carname = "Volvo" ; 
+
+
+
+//2//
+var firstName = "John"; var lastName = "Doe"; var age = 35
+
+let firstName = "John";
+let lastName = "Doe" 
+let fullname = firstname + lastname ;
+
+console.log(fullname)
+//3//
+
+x = 10;
+y = 5;
+x *= y; 
+console.log(x);
+
+//4//
+
+
+let length = 16; //number //
+
+let lastName = "Johnson"; // string //
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+}; // object //
+
+
+//5//
+
+
